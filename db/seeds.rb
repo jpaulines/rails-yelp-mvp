cedd5ff1db8eb6f1b8944b39621783c570529f0f
Judith Saragossi <judithsaragossi@gmail.com>
2019-09-03 15:15:57 +0300

    second challenge

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name: 'Pizza Lilla',
+    address: 'Levinsky St., Tel Aviv',
+    phone_number: '03-516-8244',
+    category: 'italian'
+  },
+  {
+    name: 'Onza',
+    address: 'Yaffo St., Tel Aviv',
+    phone_number: '03-516-8888',
+    category: 'belgian'
+  },
+  {
+    name: 'Le Pacifique',
+    address: 'Av. de Tervuren, Brussels',
+    phone_number: '02-640-3456',
+    category: 'chinese'
+
+  },
+  {
+    name: 'Makisu',
+    address: 'Rue du Bailli, Brussels',
+    phone_number: '02-640-3455',
+    category: 'japanese'
+  },
+  {
+    name: 'Izakaya',
+    address: 'Chaussee de Vleurgat, Brussels',
+    phone_number: '',
+    category: 'japanese'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
